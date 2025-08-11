081817a7de7789b56660b10b118242c27e9faf73
Daniel Watkins <daniel@fantasymoguls.com>
2025-08-11 16:41:43 -0700

    Additional changes

--- src/types/roster.ts
+++ src/types/roster.ts
@@ -1,27 +1,27 @@
 export interface Skills {
-  awr: number;
-  pas: number;
-  run: number;
-  skl: number;
-  spd: number;
+    awr: number;
+    pas: number;
+    run: number;
+    skl: number;
+    spd: number;
 }
 
 export interface Player {
-  id: number;
-  dateCreated: number;
-  dateModified: number;
-  firstName: string;
-  middleName: string;
-  lastName: string;
-  skills: Skills;
-  [key: string]: any; // Allow for other properties we want to preserve
+    id: number;
+    dateCreated: number;
+    dateModified: number;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    skills: Skills;
+    [key: string]: any; // Allow for other properties we want to preserve
 }
 
 export interface Roster {
-  id: number;
-  dateCreated: number;
-  dateModified: number;
-  version: number;
-  userEntryId: number;
-  players: Player[];
+    id: number;
+    dateCreated: number;
+    dateModified: number;
+    version: number;
+    userEntryId: number;
+    players: Player[];
 }
